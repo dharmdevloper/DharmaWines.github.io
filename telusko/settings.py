d97787cfcec9f1246084a24790d0badcb8c17bc2
dharmdevloper <78961675+dharmdevloper@users.noreply.github.com>
2021-02-27 13:44:36 +0530

    Update settings.py

--- telusko/settings.py
+++ telusko/settings.py
@@ -124,8 +124,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-
-#added manually
-STATICFILES_DIRS = [
-   os.path.join(BASE_DIR, "static")
-]
+if DEBUG:  
+    STATICFILES_DIRS = [
+   os.path.join(BASE_DIR, 'static')
+    ]
+else:
+   STATIC_ROOT = os.path.join(BASE_DIR,'static')
