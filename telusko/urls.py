a45f6a3f0c3b1ccc9a08192a9d1fe671c97ff79d
dharmdevloper <78961675+dharmdevloper@users.noreply.github.com>
2021-02-27 15:17:42 +0530

    New

--- telusko/urls.py
+++ telusko/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from django.views.static import serve
+from django.conf.urls import url
 
 admin.site.site_header = "Dharm Wines Admin"
 admin.site.site_title = "Dharm Wines Portal"
@@ -24,5 +28,7 @@ admin.site.index_title = "Welcome to Dharm Wines Portal"
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('calc.Urls'))
+    path('', include('calc.Urls')),
+    url(r'^media/(?P<path>.*)$', serve,{'document_root':  settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT})
 ]
