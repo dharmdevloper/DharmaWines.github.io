d9f2d34f0bea87211e9068407b2e2d12ed635aca
dharmdevloper <78961675+dharmdevloper@users.noreply.github.com>
2021-02-27 14:45:02 +0530

    commit

--- telusko/settings.py
+++ telusko/settings.py
@@ -27,9 +27,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '^7k11m06jn1_ln7rtp@sfyi5f6or6kj@&+*x5=+)98t@vr3m6_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1','dharmawines.herokuapp.com']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -46,7 +46,6 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -124,9 +123,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-if DEBUG:  
-    STATICFILES_DIRS = [
+if DEBUG:
+  STATICFILES_DIRS = [
    os.path.join(BASE_DIR, 'static')
-    ]
+  ]
 else:
    STATIC_ROOT = os.path.join(BASE_DIR,'static')
